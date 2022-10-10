0b1fb657552e6b78d2f4c1dacca2be2515c10398
SonaVarshney <sonavarshney29@gmail.com>
2022-10-10 19:29:42 +0530

    added

---
+++ C++/Kadane_algorithm.cpp
@@ -0,0 +1,31 @@
+//Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.A subarray is a contiguous part of an array.
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+int maximumSubarraySum(vector < int > arr) {
+  int n = arr.size();
+  int maxSum = INT_MIN;
+
+  for (int i = 0; i <= n - 1; i++) {
+    int currSum = 0;
+    for (int j = i; j <= n - 1; j++) {
+      currSum += arr[j];
+      if (currSum > maxSum) {
+        maxSum = currSum;
+      }
+    }
+  }
+
+  return maxSum;
+}
+//Driver's code
+int main() {
+
+    vector<int> a = {1, 3, 8, -2, 6, -8, 5};
+    cout << maximumSubarraySum(a) << endl;
+    return 0;
+}
