619c9a335020f76c33fd3bfa99180178666b3103
Aniket Surjey <surjeyaniket@gmail.com>
2022-10-19 00:43:19 +0530

    Added Code for Balanced Parenthesis using stack in cpp

---
+++ C++/balanced_parenthesis.cpp
@@ -0,0 +1,40 @@
+// C++ program to check for balanced brackets.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool Balanced(string expr)
+{
+    stack<char> temp;
+    for (int i = 0; i < expr.length(); i++)
+    {
+        if (temp.empty())
+        {
+            temp.push(expr[i]);
+        }
+        else if ((temp.top() == '(' && expr[i] == ')') || (temp.top() == '{' && expr[i] == '}') || (temp.top() == '[' && expr[i] == ']'))
+        {
+            temp.pop();
+        }
+        else
+        {
+            temp.push(expr[i]);
+        }
+    }
+    if (temp.empty())
+    {
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    string expr = "{()}[]";
+
+    if (Balanced(expr))
+        cout << "Balanced";
+    else
+        cout << "Not Balanced";
+    return 0;
+}
